154d02ef958d30cf15d9ab97695bb929334a9820
Mohit Pawar <Mohit.55pwr@gmail.com>
2022-03-24 18:47:53 +0530

    set delete controller for post and comment and distributed home.ejs code into partials

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -21,3 +21,19 @@ module.exports.create = function(req, res){
 
     });
 }
+
+module.exports.destroy=(req,res)=>{
+
+    Comment.findById(req.params.id,(err,comment)=>{
+       
+        if(comment.user==req.user.id){
+           let postId=comment.post
+            comment.remove()
+            Post.findByIdAndUpdate(postId,{$pull:{comments:req.params.id}},(err,post)=>{
+                return res.redirect('back')
+            })
+            
+
+        }
+    })
+}
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,5 +1,5 @@
 const Post=require('../models/post')
-
+const Comment=require('../models/comment')
 module.exports.create=(req,res)=>{
 
     Post.create({
@@ -14,5 +14,27 @@ module.exports.create=(req,res)=>{
       return res.redirect('back')
     });
     
+}
+
+
+module.exports.destroy=(req,res)=>{
+    Post.findById(req.params.id,(err,post)=>{
+
+            if(post.user==req.user.id){
+                post.remove()
+                Comment.deleteMany(post.user,(err)=>{
+                    if(err){
+                        console.log('error in deleting comments',err)
+                        return 
+                    }
+                    res.redirect('back')
+
+                })
+
+            }else{
+                return res.redirect('back')
+            }
+
+    })
 }
 
